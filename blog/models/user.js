f17ba1674a29a3149319cc6e3f1249e34e767880
ss <ss@hfuusec.cn>
2016-10-02 19:35:09 +0800

    写完以后，可以跑的版本

--- blog/models/user.js
+++ blog/models/user.js
@@ -1,4 +1,5 @@
 var mongodb = require('./db');
+var crypto = require('crypto');
 
 function User(user) {
   this.name = user.name;
@@ -10,11 +11,15 @@ module.exports = User;
 
 //存储用户信息
 User.prototype.save = function(callback) {
-  //要存入数据库的用户文档
+  var md5 = crypto.createHash('md5'),
+      email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+      head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
+  //要存入数据库的用户信息文档
   var user = {
       name: this.name,
       password: this.password,
-      email: this.email
+      email: this.email,
+      head: head
   };
   //打开数据库
   mongodb.open(function (err, db) {
@@ -33,7 +38,7 @@ User.prototype.save = function(callback) {
       }, function (err, user) {
         mongodb.close();
         if (err) {
-          return callback(err);//错误，返回 err 信息
+          return callback(err);
         }
         callback(null, user[0]);//成功！err 为 null，并返回存储后的用户文档
       });
@@ -60,7 +65,7 @@ User.get = function(name, callback) {
       }, function (err, user) {
         mongodb.close();
         if (err) {
-          return callback(err);//失败！返回 err 信息
+          return callback(err);//失败！返回 err
         }
         callback(null, user);//成功！返回查询的用户信息
       });
